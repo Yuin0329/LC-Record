563269fdbfbe6579afb25989c635f6c50e10acc4
Yuin0329 <95572957+Yuin0329@users.noreply.github.com>
2026-04-04 04:27:22 +0800

    Time: 0 ms (100%), Space: 8.2 MB (95.95%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) 
+    {
+    vector<double> ans(2);
+    double k=celsius+273.15;
+    double f=celsius*1.80+32.00;
+    ans[0]=k;
+    ans[1]=f;
+    return(ans);
+    }
+};
