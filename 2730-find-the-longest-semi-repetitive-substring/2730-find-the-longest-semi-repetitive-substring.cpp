609935f5479c2df1e08f317979208527404c07c4
Yuin0329 <95572957+Yuin0329@users.noreply.github.com>
2026-04-12 00:15:54 +0800

    Time: 0 ms (100%), Space: 10.4 MB (26.92%) - LeetHub

--- 2730-find-the-longest-semi-repetitive-substring/2730-find-the-longest-semi-repetitive-substring.cpp
+++ 2730-find-the-longest-semi-repetitive-substring/2730-find-the-longest-semi-repetitive-substring.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int longestSemiRepetitiveSubstring(string s) {
-        int ans=0,left=0,n=s.size(),cnt0=0,cnt1=0;
+        int ans=0,left=0,n=s.size(),cnt0=0;
 
         for(int right=0;right<n;right++){
             if(right>0 && s[right-1]==s[right]) cnt0++;
