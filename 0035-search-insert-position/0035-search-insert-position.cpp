bbafa80310a0f299063cbb3f0334eb17d4fd1ef0
Yuin0329 <95572957+Yuin0329@users.noreply.github.com>
2026-05-08 10:24:49 +0800

    Time: 0 ms (100%), Space: 13.5 MB (76.46%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -3,8 +3,8 @@ int lowerbound(vector<int>& nums, int target){
     int left=-1,right=nums.size();
     while(left+1<right){
         int mid=left+(right-left)/2;
-        if(nums[mid]<target) left=mid;
-        else right=mid;
+        if(nums[mid]>=target) right=mid;
+        else left=mid;
     }
     return right;
 };
