bc7683b0dcfbd923ed37d1b2980c8ef50ba75cb7
Yuin0329 <95572957+Yuin0329@users.noreply.github.com>
2026-05-12 22:09:04 +0800

    Time: 0 ms (100%), Space: 21.5 MB (25.29%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,12 +1,22 @@
 class Solution {
+int lower_bound(vector<int>& nums,int target){
+    int left=-1,right=nums.size();
+        while(left+1 < right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]>=target) right=mid;
+            else left=mid;
+        }
+        return right;
+}
+
 public:
     int maximumCount(vector<int>& nums) {
-        int neg=0,pos=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]<0) neg++;
-            else if(nums[i]>0) pos++; 
-        }
+        int n=nums.size();
+        int neg = lower_bound(nums, 0); // 第一個 >= 0 的位置 = 負數數量
+        int pos = n - lower_bound(nums, 1); // 第一個 >= 1 的位置後面都是正數
+        if(nums[0] >0 || nums[n-1] <0 ) return n;
         return max(neg,pos);
         
-    }
+}
+
 };
