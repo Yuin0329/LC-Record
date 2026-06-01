74ad2d003ed1a8c4a4ddc4be54844792f6ed6758
Yuin0329 <95572957+Yuin0329@users.noreply.github.com>
2026-06-01 09:16:26 +0800

    Time: 51 ms (50.99%), Space: 91 MB (27.85%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        bool ans=0;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+            if( mp[nums[i]]>1){
+                ans=1;
+                break;
+            }
+        } 
+        return ans;
+    }
+};
